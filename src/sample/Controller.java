30a44391736e28ed658b7b4417438a004734391f
EveIsSim <eve.is.sim@gmail.com>
2021-05-12 02:47:41 +0300

    paint view

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,16 @@
 package sample;
 
+import javafx.fxml.FXML;
+
+import javafx.scene.control.TextArea;
+import javafx.scene.control.Button;
+
 public class Controller {
+
+    @FXML
+    TextArea mainTextArea;
+
+    public void btnClicked(javafx.event.ActionEvent actionEvent) {
+        mainTextArea.setText("text");
+    }
 }
